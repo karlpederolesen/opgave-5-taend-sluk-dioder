fac79950314f0a00816ae6acd1ee9aa6b55bfd59
karlpederolesen <96655854+karlpederolesen@users.noreply.github.com>
2021-12-26 16:25:57 +0100

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,33 @@
+input.onButtonPressed(Button.A, function () {
+    led.plot(0, 0)
+    led.plot(1, 1)
+    led.plot(2, 2)
+    led.plot(3, 3)
+    led.plot(4, 4)
+})
+input.onButtonPressed(Button.AB, function () {
+    pause2 = !(pause2)
+    if (pause2) {
+        basic.clearScreen()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    led.unplot(0, 0)
+    led.unplot(1, 1)
+    led.unplot(2, 2)
+    led.unplot(3, 3)
+    led.unplot(4, 4)
+})
+let yPos = 0
+let xPos = 0
+let pause2 = false
+pause2 = false
 basic.forever(function () {
-	
+    if (!(pause2)) {
+        xPos = randint(0, 4)
+        yPos = randint(0, 4)
+        led.plot(xPos, yPos)
+        basic.pause(10)
+        led.unplot(yPos, xPos)
+    }
 })
